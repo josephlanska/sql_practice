141f12494a8b12a657f81d0338b29735e752085a
josephlanska <josephlanska@users.noreply.github.com>
2019-04-09 13:35:15 -0700

    Add column aliases

--- northwind/problems.sql
+++ northwind/problems.sql
@@ -3,7 +3,7 @@
 -- What is the name of the customer who has the most orders?
 
 SELECT
-  Customers.CustomerName,
+  Customers.CustomerName AS customer_name,
   COUNT(*) AS num_orders
 FROM Customers
 INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
@@ -17,12 +17,12 @@ LIMIT 1;
 -- Which supplier has the highest average product price?
 
 SELECT
-  Suppliers.SupplierName,
-  AVG(Products.Price) AS avg_price
+  Suppliers.SupplierName AS supplier_name,
+  AVG(Products.Price) AS avg_product_price
 FROM Suppliers
 INNER JOIN Products ON Products.SupplierID = Suppliers.SupplierID
 GROUP BY Suppliers.SupplierID
-ORDER BY avg_price DESC
+ORDER BY avg_product_price DESC
 
 LIMIT 1;
 
@@ -31,7 +31,7 @@ LIMIT 1;
 -- How many different countries are the customers from?
 
 SELECT
-  COUNT(DISTINCT(Country)) as num_countries
+  COUNT(DISTINCT(Customers.Country)) as num_countries
 FROM Customers;
 
 -- -----------------------------------------------------------------------------
@@ -39,7 +39,7 @@ FROM Customers;
 -- What category appears in the most orders?
 
 SELECT
-  Categories.CategoryName,
+  Categories.CategoryName AS category_name,
   COUNT(*) AS num_orders
 FROM Categories
 INNER JOIN Products ON Products.CategoryID = Categories.CategoryID
@@ -54,11 +54,11 @@ LIMIT 1;
 -- What was the total cost for each order?
 
 SELECT
-  OrderDetails.OrderID,
+  OrderDetails.OrderID AS order_id,
   ROUND(SUM(Products.Price * OrderDetails.Quantity), 2) as total_order_cost
 FROM OrderDetails
 INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID
-GROUP BY OrderDetails.OrderID
+GROUP BY order_id
 ORDER BY total_order_cost DESC;
 
 -- -----------------------------------------------------------------------------
@@ -66,8 +66,8 @@ ORDER BY total_order_cost DESC;
 -- Which employee made the most sales (by total cost)?
 
 SELECT
-  Employees.FirstName,
-  Employees.LastName,
+  Employees.LastName AS employee_lastname,
+  Employees.FirstName AS employee_firstname,
   ROUND(SUM(Products.Price * OrderDetails.Quantity), 2) AS total_employee_sales
 FROM Employees
 INNER JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
@@ -83,9 +83,8 @@ LIMIT 1;
 -- Which employees have BS degrees?
 
 SELECT
-  Employees.LastName,
-  Employees.FirstName,
-  Employees.Notes
+  Employees.LastName AS employee_lastname,
+  Employees.FirstName AS employee_firstname
 FROM Employees
 WHERE Employees.Notes LIKE '%BS%';
 
@@ -94,7 +93,7 @@ WHERE Employees.Notes LIKE '%BS%';
 -- Which supplier of 3 or more products has the highest average product price?
 
 SELECT
-  Suppliers.SupplierName,
+  Suppliers.SupplierName AS supplier_name,
   COUNT(*) AS num_products,
   ROUND(AVG(Products.Price), 2) AS avg_product_price
 FROM Suppliers
